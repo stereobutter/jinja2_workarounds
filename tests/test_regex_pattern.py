ead2c779919006ce3fe58f07dd83ad84dad87b27
Sascha Desch <sascha.desch@hotmail.com>
2022-03-13 18:49:32 +0100

    add tests for regex

--- tests/test_regex_pattern.py
+++ tests/test_regex_pattern.py
@@ -1,6 +1,25 @@
 import pytest
 import re
-from jinja2_better_includes import _include_statement
+from jinja2_better_includes import _improved_include_statement
+
+
+@pytest.mark.parametrize("tags", [("{%", "%}"), ("<<", ">>"), ("|$", "$|")])
+@pytest.mark.parametrize("statement",
+    [
+        '{start} include "foo.txt" with indentation {end}', 
+        "{start} include 'foo.txt' with indentation {end}",
+        "{start}include'foo.txt'with indentation{end}"
+        "{start}  include  ['foo.txt', 'bar.txt'] ignore missing with indentation {end}",
+        "{start}  include  ['foo.txt', 'bar.txt'] ignore missing with indentation with context {end}",
+        "     {start}  include  'foo.txt' with indentation {end}",
+        "  ...   {start}  include  'foo.txt' with indentation {end} .. ",
+        "{start}- include 'foo.txt' with indentation +{end}"
+    ]
+)
+def test_regex_matches(tags, statement):
+    start, end = tags
+    string = statement.format(start=start, end=end)
+    assert _improved_include_statement(start, end).search(string)
 
 
 @pytest.mark.parametrize("tags", [("{%", "%}"), ("<<", ">>"), ("|$", "$|")])
@@ -8,13 +27,10 @@ from jinja2_better_includes import _include_statement
     [
         '{start} include "foo.txt" {end}', 
         "{start} include 'foo.txt' {end}",
-        "{start}include'foo.txt'{end}"
-        "{start}  include  ['foo.txt', 'bar.txt'] ignore missing  {end}",
-        "     {start}  include  'foo.txt'  {end}"
-        "  foo:   {start}  include  'foo.txt'  {end} bar "
+        "{start} include 'foo.txt' with context{end}"
     ]
 )
-def test_regex_matches(tags, statement):
+def test_regex_doesnt_matches(tags, statement):
     start, end = tags
     string = statement.format(start=start, end=end)
-    assert _include_statement(start, end).search(string)
+    assert not _improved_include_statement(start, end).search(string)
